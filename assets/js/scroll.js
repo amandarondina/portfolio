05aa66ab063916af5097a151c09552a26e92b692
Amanda <contact.amandarondina@gmail.com>
2023-07-26 14:01:28 +0200

    first commit

---
+++ assets/js/scroll.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.avatar', {duration : 3000, origin: 'right'})
+sr.reveal('.my-name', {duration : 3000, origin: 'left'})
+sr.reveal('.portfolio', {duration : 3000,  origin: 'bottom'})
+sr.reveal('.about-me', {duration : 3000, origin:'right'})
+sr.reveal('.cards__item', {duration : 3000, origin:'bottom'})
+sr.reveal('.projects', {duration : 3000, origin:'bottom'})
+sr.reveal('.icons', {duration : 3000, origin:'bottom'})
+sr.reveal('.contacts', {duration : 3000, origin:'right'})
