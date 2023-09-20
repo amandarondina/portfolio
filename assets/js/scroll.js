d136cfef53417c240b3df5dfda78bab0a98e4821
Amanda <contact.amandarondina@gmail.com>
2023-09-20 15:41:33 +0200

    "style update"

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,10 +1,12 @@
-window.sr = ScrollReveal({ reset:true});
-ScrollReveal({ distance: '15px' });
-sr.reveal('.avatar', {duration : 3000, origin: 'right'})
-sr.reveal('.my-name', {duration : 3000, origin: 'left'})
-sr.reveal('.portfolio', {duration : 3000,  origin: 'bottom'})
-sr.reveal('.about-me', {duration : 3000, origin:'right'})
-sr.reveal('.cards__item', {duration : 3000, origin:'bottom'})
-sr.reveal('.projects', {duration : 3000, origin:'bottom'})
-sr.reveal('.icons', {duration : 3000, origin:'bottom'})
-sr.reveal('.contacts', {duration : 3000, origin:'right'})
+window.sr = ScrollReveal({ reset: true });
+ScrollReveal({ distance: "15px" });
+sr.reveal(".avatar", { duration: 3000, origin: "right" });
+sr.reveal(".my-name", { duration: 3000, origin: "left" });
+sr.reveal(".portfolio", { duration: 6000, origin: "left" });
+sr.reveal(".title", { duration: 3000, origin: "bottom" });
+sr.reveal(".about-me", { duration: 3000, origin: "right" });
+sr.reveal(".cards__item", { duration: 3000, origin: "right" });
+sr.reveal(".icons", { duration: 3000, origin: "bottom" });
+sr.reveal(".email", { duration: 3000, origin: "right" });
+sr.reveal(".logo-github", { duration: 3000, origin: "right" });
+sr.reveal(".logo-linkedin", { duration: 3000, origin: "left" });
